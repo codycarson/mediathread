70529ed0341bb287a72a757e852a5b5c82c51b34
Filip Jukic <filip@jukic.me>
2013-08-21 12:52:12 -0400

    tests for resend invite view

--- mediathread/course/tests/test_views.py
+++ mediathread/course/tests/test_views.py
@@ -1,5 +1,6 @@
 import analytics
 from django.contrib.auth.models import User
+from django.core import mail
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from courseaffils.models import Course
@@ -66,3 +67,27 @@ class PromoteStudentTest(TestCase):
         self.assertFalse(user in course.faculty_group.user_set.all(), response)
         self.assertRedirects(response, reverse('member_list'))
         self.assertContains(response, "You must be an instructor in this course to do that")
+
+
+class ResendInviteTest(TestCase):
+    fixtures = ['unittest_sample_course.json', 'registration_data.json']
+
+    def setUp(self):
+        self.client.login(username="test_instructor", password="test")
+
+    def test_resend_invite_to_nonactive_user(self):
+        response = self.client.post(reverse("resend_invite"), {
+            'user_id': 3
+        }, follow=True)
+        user = User.objects.get(id=3)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertRedirects(response, reverse('member_list'))
+        self.assertContains(response, "successfully sent the activation email to {0}".format(user.email))
+
+    def test_resend_invite_to_active_user(self):
+        response = self.client.post(reverse("resend_invite"), {
+            'user_id': 4
+        }, follow=True)
+        self.assertEqual(len(mail.outbox), 0)
+        self.assertRedirects(response, reverse('member_list'))
+        self.assertContains(response, "User already activated his account.")
