ffa36060801c815ced530d71159dc533653d745a
LittleQ <littleq0903@gmail.com>
2013-08-22 23:24:18 +0800

    2.0 tests, did not get mock working

--- mediathread/user_accounts/tests/test_utils.py
+++ mediathread/user_accounts/tests/test_utils.py
@@ -3,10 +3,10 @@ from django.conf import settings
 
 from mediathread.user_accounts.utils import add_email_to_mailchimp_list
 
-import mailsnake
+import mailchimp
 from mock import patch, MagicMock
 
-mock_mailsnake = MagicMock(spec=mailsnake)
+mock_mailchimp = MagicMock(spec=mailchimp)
 
 """
 Exceptions
@@ -24,15 +24,19 @@ class ListIdNotValid(Exception):
 Test Cases
 """
 
-@patch("mailsnake.MailSnake", mock_mailsnake)
+
+#@patch("mailchimp.Mailchimp", mock_mailchimp)
 class MailChimpTest(TestCase):
     def setUp(self):
         self.test_mailchimp_data = {
             'list_id': settings.MAILCHIMP_REGISTRATION_LIST_ID,
             'api_key': settings.MAILCHIMP_API_KEY
             }
+
+        print 'Using list id: %s' % self.test_mailchimp_data['list_id']
+        print 'Using api key: %s' % self.test_mailchimp_data['api_key']
         self.test_mailchimp_fields = {
-            u'EMAIL': u'littleq0903+test@gmail.com',
+            u'EMAIL': u'mediathreadtest@mediathread.com',
             u'FNAME': u'Tester',
             u'LNAME': u'Su',
             u'HEAR': u'Conference',
@@ -40,18 +44,19 @@ class MailChimpTest(TestCase):
             u'TITLE': u'student'
             }
 
-        self.ms = mailsnake.MailSnake(self.test_mailchimp_data['api_key'])
+        self.ms = mailchimp.Mailchimp(self.test_mailchimp_data['api_key'])
 
     def test_api_key_valid(self):
         try:
-            self.ms.lists()
+            self.ms.lists.list()
         except:
             raise ApiKeyNotValid("Mailchimp API key is not valid")
 
     def test_registration_list_id_valid(self):
         try:
-            self.ms.listMergeVars(id=self.test_mailchimp_data['list_id'])
-        except:
+            self.ms.lists.merge_vars(id=[self.test_mailchimp_data['list_id']])
+        except Exception as e:
+            print e
             raise ListIdNotValid("Given list id is not valid")
 
     def test_subscribe_user(self):
@@ -61,13 +66,16 @@ class MailChimpTest(TestCase):
 
         test_fields_arg = dict([(k.lower(), v) for k, v in test_fields.items()])
         result = add_email_to_mailchimp_list(test_fields['EMAIL'], test_data['list_id'], **test_fields_arg)
+        test_email_struct = {
+            'email': test_fields['EMAIL']
+        }
         self.assertEqual(result, True)
 
         # check whether user exists in list
-        result_memberinfo = self.ms.listMemberInfo(id=test_data['list_id'], email_address=[test_fields['EMAIL']])
+        result_memberinfo = self.ms.lists.member_info(id=test_data['list_id'], emails=[test_email_struct])
         from pprint import pprint
         pprint(result_memberinfo)
-        self.assertEqual(result_memberinfo['errors'], 0)
+        self.assertEqual(result_memberinfo['error_count'], 0)
 
         # check user information is correct
         result_member = result_memberinfo['data'][0]
@@ -79,10 +87,11 @@ class MailChimpTest(TestCase):
         """
         self.assertDictEqual(test_fields, result_member_merges)
 
+
         # roll back
-        self.ms.listUnsubscribe(
+        self.ms.lists.unsubscribe(
             id=test_data['list_id'],
-            email_address=test_fields['EMAIL'],
+            email=test_email_struct,
             delete_member=True,
             send_goodbye=False,
             send_notify=False)
