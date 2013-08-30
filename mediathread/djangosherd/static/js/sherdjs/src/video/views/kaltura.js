d77ccc494ac235ae01d7389fb3e8e97dbd8eb56d
Filip Jukic <filip@jukic.me>
2013-08-30 18:27:05 -0400

    kaltura video temporary fix

--- mediathread/djangosherd/static/js/sherdjs/src/video/views/kaltura.js
+++ mediathread/djangosherd/static/js/sherdjs/src/video/views/kaltura.js
@@ -2,7 +2,6 @@
   Support for the Kaltura js-enabled player.  documentation at:
   http://Kaltura.com/api/docs/oembed
  */
-
 if (!Sherd) { Sherd = {}; }
 if (!Sherd.Video) { Sherd.Video = {}; }
 if (!Sherd.Video.Kaltura && Sherd.Video.Base) {
@@ -86,13 +85,13 @@ if (!Sherd.Video.Kaltura && Sherd.Video.Base) {
                 mediaUrl: url, // Used by _seek seeking behavior
                 text: '<div id="' + wrapperID + '" class="sherd-kaltura-wrapper">' +
                       '  <object width="' + obj.options.width + '" height="' + obj.options.height + '" ' + objectID +
-                          ' type="application/x-shockwave-flash" data="' + url + '" ' + classID + '>' +
+                          ' type="application/x-shockwave-flash" data="' + url+ '" ' + classID + ' name="' + playerID +
+                        '" allowFullScreen="true" allowNetworking="all" allowScriptAccess="always" resource="' + url + '" >' +
                         '  <param name="movie" value="' + url + '"></param>' +
                         '  <param name="allowscriptaccess" value="always"/></param>' +
                         '  <param name="width" value="' + obj.options.width + '"></param>' +
                         '  <param name="height" value="' + obj.options.height + '"></param>' +
                         '  <param name="allowfullscreen" value="true"></param>' +
-                        '  <param name="flashVars" value="autoPlay=' + autoplay + '&streamerType=hdnetwork&akamaiHD.loadingPolicy=preInitialize&akamaiHD.asyncInit=true&kml=local&kmlPath=http://mediathread.appsembler.com/site_media/js/sherdjs/lib/kaltura/config.xml"/>' +
                         '</object>' +
                       '</div>'
             };
